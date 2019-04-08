7a83f1040c40a21281ce8e28f51152dd4a58807e
yudong2015 <2091381910@qq.ccom>
2019-04-08 14:31:17 +0800

    refactor config.json for helm-chart

--- scripts/render-and-deploy-helm-chart.sh
+++ scripts/render-and-deploy-helm-chart.sh
@@ -8,11 +8,9 @@ else
     VERSION=${TRAVIS_TAG}
 fi
 
-sed "s,VERSION,${VERSION},g" ./templates/values.json.tmpl > ./templates/values.json
-
 # At first, you should install handlerbars impl with by pybars3, git-repo: git@github.com:wbond/pybars3.git
-python ./scripts/handlebars-renderer.py -i ./templates/values.yaml.handlebars -v ./templates/values.json -o ./openpitrix/values.yaml
-python ./scripts/handlebars-renderer.py -i ./templates/Chart.yaml.handlebars -v ./templates/values.json -o ./openpitrix/Chart.yaml
+python ./scripts/handlebars-renderer.py -i ./templates/values.yaml.handlebars -v ./templates/config.json -o ./openpitrix/values.yaml
+python ./scripts/handlebars-renderer.py -i ./templates/Chart.yaml.handlebars -v ./templates/config.json -o ./openpitrix/Chart.yaml
 
 sudo helm install -n openpitrix openpitrix
 
